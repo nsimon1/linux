34338de87ebcfd9cb9fef7ae12dfd8bcee877033
Dave Stevenson <dave.stevenson@raspberrypi.org>
2018-02-21 14:03:40 +0000

    staging: bcm2835-camera: do not initialise statics to 0
    Fix up checkpatch error "do not initialise statics to 0"
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.org>

--- drivers/staging/vc04_services/bcm2835-camera/bcm2835-camera.c
+++ drivers/staging/vc04_services/bcm2835-camera/bcm2835-camera.c
@@ -68,7 +68,7 @@ MODULE_PARM_DESC(max_video_width, "Threshold for video mode");
 module_param(max_video_height, int, 0644);
 MODULE_PARM_DESC(max_video_height, "Threshold for video mode");
 
-static int always_remove_padding = 0;
+static int always_remove_padding;
 module_param(always_remove_padding, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 MODULE_PARM_DESC(always_remove_padding,
 	"Always remove all padding from the image (for apps that ignore bytesperline)");
